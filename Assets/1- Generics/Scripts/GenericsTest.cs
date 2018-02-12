fa4ca21f65a710f1dee7a3f8395475919ff94057
Ghost72nd <calvinkeating72@gmail.com>
2018-02-12 16:06:19 +1100

    generics

---
+++ Assets/1- Generics/Scripts/GenericsTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Generics
+{
+    public class GenericsTest : MonoBehaviour
+    {
+        public GameObject prefab;
+        public int spawnAmount = 20;
+        public float spawnRadius = 50f;
+        public CustomList<float> gameObjects = new CustomList<float>();
+        // Use this for initialization
+        void Start()
+        {
+            gameObjects.Add();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
